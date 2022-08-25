cfd6050b8aa95e0fef5312d42f518246e5f664a6
Yunho <boyjcy@naver.com>
2022-08-25 19:23:18 +0900

    Spring Security 설정

--- src/main/java/com/doeg/services/member/MemberService.java
+++ src/main/java/com/doeg/services/member/MemberService.java
@@ -1,4 +1,46 @@
 package com.doeg.services.member;
 
-public class MemberService {
+import com.doeg.dto.member.MemberDto;
+import com.doeg.entity.member.Member;
+import com.doeg.entity.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class MemberService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
+        Optional<Member> memberEntityWrapper = memberRepository.findByLoginId(loginId);
+        Member member = memberEntityWrapper.orElse(null);
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        authorities.add(new SimpleGrantedAuthority("ROME_MEMBER"));
+
+        return new User(member.getLoginId(), member.getPassword(), authorities);
+    }
+
+    @Transactional
+    public Integer save(MemberDto memberDto){
+        Member member = memberDto.toEntity();
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        member.setPassword(passwordEncoder.encode(member.getPassword()));
+        return memberRepository.save(member).getMemberId();
+    }
 }
